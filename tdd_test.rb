5bb523333c044671a89ae74e53097de037d5370f
William Mullins <twfdisturbed@hotmail.com>
2018-07-11 14:02:24 -0400

    Update:

--- tdd_test.rb
+++ tdd_test.rb
@@ -6,6 +6,10 @@ class TestCodes < Minitest::Test
     def test_assert_that_1_equals_1
         assert_equal(1, 1)
     end
+    def test_if_hash_is_empty
+        coins = {}
+        assert_equal(coins, {})
+    end
     def test_if_quarter_is_25()
         coins = {:quarter => "25"}
         assert_equal(coins[:quarter], "25")
