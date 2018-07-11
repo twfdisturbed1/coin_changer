f655ed8045ec439b0c410ce127db251402c23a57
William Mullins <twfdisturbed@hotmail.com>
2018-07-11 14:09:35 -0400

    added penny to hash

--- coin_changer.rb
+++ coin_changer.rb
@@ -1,4 +1,5 @@
-coins = {:quarter => "25", :dime => "10", :nickle => "05"}
+coins = {:quarter => "25", :dime => "10", :nickle => "05", :penny => "01"}
 puts coins[:quarter]
 puts coins[:dime]
 puts coins[:nickle]
+puts coins[:penny]
