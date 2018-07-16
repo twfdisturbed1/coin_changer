97b3fdffaeaa090b9a2b7d779a72949f61b6fcf8
William Mullins <twfdisturbed@hotmail.com>
2018-07-16 12:44:36 -0400

    I don't know

--- coin_changer.rb
+++ coin_changer.rb
@@ -1,5 +1,18 @@
-coins = {:quarter => "25", :dime => "10", :nickle => "05", :penny => "01"}
-puts coins[:quarter]
-puts coins[:dime]
-puts coins[:nickle]
-puts coins[:penny]
+
+class Coin_changer
+    def final(amount)
+     coins = {:quarter => 25, :dime => 10, :nickle => 05, :penny => 01}
+     change = []         
+      remaining_amount = amount
+      coins.each do |coin|
+        if ((remaining_amount/coin).to_int > 0)
+        then (remaining_amount/coin).to_int.times { change << coin }
+          puts "Remaining Amount: #{remaining_amount} | Coin: #{coin}"
+          remaining_amount = amount - change.inject(:+)
+        end 
+    end                         
+     puts "Amount #{amount} >> Coin_changer: #{change}\n\n"
+      return change
+    end
+end
+
